b0ad2aa29fc85bb237e230831b3dbcdd4245dbf9
The-Sagebrush-Sage <Wermut-Kraft@yandex.ru>
2026-01-07 22:04:53 +0300

    Add files via upload

--- beta/bd.js
+++ beta/bd.js
@@ -72,6 +72,33 @@ const userDatabase = {
         profileDescription: "Рисую своих скрепышей и не только! <strong> Арт статус: </strong>  Коммишки - нет, Трейды - да, Ивенты - да, Коллабы - да, Реквесты - нет",
         basicInfoLocked: true,
         lastUpdated: "2026-01-05T16:37:39.721Z"
+    }, 
+
+    "user_neko_nati_zv5g": {
+        username: "user_neko_nati_zv5g",
+        profileName: "Neko Nati",
+        profileNickname: "Neko_Nati",
+        profileStyle: "Аниме",
+        avatar: "https://i.postimg.cc/261Lybxx/%D0%91%D0%B5%D0%B7_%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F323_20251231171542.png",
+        banner: "",
+        socialLinks: [
+            { platform: "telegram", username: "no_salvation_just_survive" }
+        ],
+        works: [
+            "https://i.postimg.cc/kg7V8hct/IMG_20251204_170302_440.png",
+            "https://i.postimg.cc/vHFx8mxX/%D0%91%D0%B5%D0%B7_%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F321_20251230171201.png",
+            "https://i.postimg.cc/261Lybxx/%D0%91%D0%B5%D0%B7_%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F323_20251231171542.png",
+            "https://i.postimg.cc/1zB5Smhr/2025100301214754.jpg",
+            "https://i.postimg.cc/4y7JppKr/%D0%91%D0%B5%D0%B7_%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F326_20260103160650.png",
+            "https://i.postimg.cc/tJxjsywJ/%D0%91%D0%B5%D0%B7_%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F138_20250723005903.png",
+            "https://i.postimg.cc/3Jjw1CfK/%D0%91%D0%B5%D0%B7_%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F202_20251012184416.png",
+            "https://i.postimg.cc/L8N8K4M0/%D0%91%D0%B5%D0%B7_%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F306_20251221100925.png"
+        ],
+        publishedOn: "prem_artist",
+        userPassword: "Y9Cjea5zR",
+        profileDescription: "Рисую своих скрепышей и не только! <strong> Арт статус: </strong>  Коммишки - нет, Трейды - да, Ивенты - да, Коллабы - да, Реквесты - нет",
+        basicInfoLocked: true,
+        lastUpdated: "2026-01-05T16:37:39.721Z"
     }
 
 
